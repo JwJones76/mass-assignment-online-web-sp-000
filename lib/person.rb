62bb713125336ea51d04dd4ee876ac2bc20e96a4
Jacob W Jones <jacobwadejones76@gmail.com>
2021-04-13 20:37:38 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,3 @@
-lines (10 sloc)  318 Bytes
-  
 class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   def initialize(attributes=nil)
